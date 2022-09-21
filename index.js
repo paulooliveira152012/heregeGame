94875a1c3db1082e1391eec902954217b763bfd3
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-09-20 22:00:10 -0400

    refactor for loop applying DRY

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function start() {
     // //characters card
 
     // //array with all the images
-    var charactersImages = new Array ("photos/cold.jpg", "photos/david.jpg", "photos/freirinha.jpg", "photos/itachi.jpg")
+    var charactersImages = new Array ("photos/cold.jpg", "photos/david.jpg", "photos/freirinha.jpg", "photos/itachi.jpg", "photos/kousei.jpg", "photos/alexandre.jpg")
     
     //function to randomly choose an image
     function choosePic() {
@@ -86,13 +86,23 @@ function start() {
         console.log(randomNum)
 
         if(randomNum == 0) {
-            window.alert('cold')
+            // window.alert('cold')
+            coldInfo()
         }else if(randomNum == 1) {
-            window.alert('david')
+            // window.alert('david')
+            davidInfo()
         }else if(randomNum == 2) {
-            window.alert('freirinha')
+            // window.alert('freirinha')
+            freirinhaInfo()
         }else if(randomNum == 3) {
-            window.alert('itachi')
+            // window.alert('itachi')
+            itachiInfo()
+        }else if(randomNum == 4) {
+            // window.alert('Kousei')
+            kouseiInfo()
+        }else if(randomNum == 5) {
+            // window.alert('Alexandre')
+            alexandreInfo()
         }
 
         topSection.appendChild(imagePlaceHolder)
@@ -110,9 +120,81 @@ function start() {
     // bottomSection.appendChild(card)
 
 
+
+    //function for loop
+    function loopAttributes() {
+        for(let i = 0; i < attributes.length; i++) {
+            const list = document.createElement('li')
+            // list.style.innerHTML = attributes.length
+            list.innerText = attributes[i];
+    
+            list.style.color= 'black'
+            list.style.backgroundColor=('yellow')
+    
+            //append list to bottomSection
+            bottomSection.appendChild(list)
+        }
+    }
+   
+
+
     //function to add info to cold
     function coldInfo() {
-        const attributes = [`Heresia = ${100} `, `Grego = ${70}`, `Teologia = ${400}`, `Historicidade = ${300}`, `Uncao = ${50}`]
+        const attributes = [`Heresia = ${150} `, `Grego = ${0}`, `Sofismo = ${700}`, `Idolatria = ${100}`, `Uncao = ${50}`, `Indignacao = ${0}`]
+
+        // for(let i = 0; i < attributes.length; i++) {
+        //     const list = document.createElement('li')
+        //     // list.style.innerHTML = attributes.length
+        //     list.innerText = attributes[i];
+
+        //     list.style.color= 'black'
+        //     list.style.backgroundColor=('yellow')
+
+        //     //append list to bottomSection
+        //     bottomSection.appendChild(list)
+        // }
+
+        loopAttributes()
+    }
+
+    
+    // function to add info to david
+    function davidInfo() {
+        const attributes = [`Heresia = ${700} `, `Grego = ${0}`, `Sofismo = ${50}`, `Idolatria = ${100}`, `Uncao = ${50}`, `Indignacao = ${100}`]
+
+        for(let i = 0; i < attributes.length; i++) {
+            const list = document.createElement('li')
+            // list.style.innerHTML = attributes.length
+            list.innerText = attributes[i];
+
+            list.style.color= 'black'
+            list.style.backgroundColor=('yellow')
+
+            //append list to bottomSection
+            bottomSection.appendChild(list)
+        }
+    }
+
+    //function to add info to freirinha
+    function freirinhaInfo() {
+        const attributes = [`Heresia = ${200} `, `Grego = ${0}`, `Sofismo = ${100}`, `Idolatria = ${700}`, `Uncao = ${50}`, `Indignacao = ${50}`]
+
+        for(let i = 0; i < attributes.length; i++) {
+            const list = document.createElement('li')
+            // list.style.innerHTML = attributes.length
+            list.innerText = attributes[i];
+
+            list.style.color= 'black'
+            list.style.backgroundColor=('yellow')
+
+            //append list to bottomSection
+            bottomSection.appendChild(list)
+        }
+    }
+
+     //function to add info to freirinha
+     function kouseiInfo() {
+        const attributes = [`Heresia = ${100} `, `Grego = ${500}`, `Sofismo = ${200}`, `Idolatria = ${100}`, `Uncao = ${100}`, `Indignacao = ${0}`]
 
         for(let i = 0; i < attributes.length; i++) {
             const list = document.createElement('li')
@@ -127,23 +209,41 @@ function start() {
         }
     }
     
-    coldInfo()
+        //function to add info to itachi
+        function itachiInfo() {
+            const attributes = [`Heresia = ${100} `, `Grego = ${0}`, `Sofismo = ${100}`, `Idolatria = ${0}`, `Uncao = ${700}`, `Indignacao = ${100}`]
     
+            for(let i = 0; i < attributes.length; i++) {
+                const list = document.createElement('li')
+                // list.style.innerHTML = attributes.length
+                list.innerText = attributes[i];
     
+                list.style.color= 'black'
+                list.style.backgroundColor=('yellow')
+    
+                //append list to bottomSection
+                bottomSection.appendChild(list)
+            }
+        }
+
+          //function to add info to Alexandre
+          function alexandreInfo() {
+            const attributes = [`Heresia = ${130} `, `Grego = ${0}`, `Sofismo = ${100}`, `Idolatria = ${100}`, `Uncao = ${70}`, `Indignacao = ${600}`]
+    
+            for(let i = 0; i < attributes.length; i++) {
+                const list = document.createElement('li')
+                // list.style.innerHTML = attributes.length
+                list.innerText = attributes[i];
+    
+                list.style.color= 'black'
+                list.style.backgroundColor=('yellow')
+    
+                //append list to bottomSection
+                bottomSection.appendChild(list)
+            }
+        }
 
 
 choosePic()
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
